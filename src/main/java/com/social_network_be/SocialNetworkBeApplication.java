2109198f3b6cf4ef3560f55b3a540f2978ee26a9
Twilight0908 <nguyenquang00.it@gmail.com>
2023-10-03 08:59:07 +0700

    update file

---
+++ src/main/java/com/social_network_be/SocialNetworkBeApplication.java
@@ -0,0 +1,13 @@
+package com.social_network_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/social_network_be/SocialNetworkBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social_network_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
